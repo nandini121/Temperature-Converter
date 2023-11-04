31dbd236dd1374fbb4e13ca4ec904e484e928d51
HP <nandini.brhmarishi999@gmail.com>
2023-11-04 22:24:41 +0530

    Final Commit

---
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.temperatureconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cToF, fToC;
+    private TextView result;
+    private EditText enterTemp;
+    double result0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cToF = findViewById(R.id.cToF);
+        fToC = findViewById(R.id.fToC);
+        result = findViewById(R.id.result);
+        enterTemp = findViewById(R.id.enterTemp);
+
+        cToF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double temp = Double.parseDouble(enterTemp.getText().toString());
+                result0 = (temp *1.8) + 32;
+                result.setText(String.valueOf(result0));
+
+            }
+        });
+        fToC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double temp = Double.parseDouble(enterTemp.getText().toString());
+                result0 = (temp - 32) / 1.8;
+                result.setText(String.valueOf(result0));
+            }
+        });
+    }
+}
